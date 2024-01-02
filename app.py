0d21eeca60a159414cc70870d15788cef5fd4572
honlnm <nicole.m.honl@gmail.com>
2024-01-02 09:41:57 -0600

    finished exercise

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
-from forms import AddPet
+from forms import PetInfo, EditPetInfo
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -37,15 +37,38 @@ def show_pets():
 @app.route('/add', methods=["GET", "POST"])
 def add_pet():
     """add pet to the 'pets' table"""
-    form = AddPet()
+    form = PetInfo()
     if form.validate_on_submit():
         name = form.name.data
         species = form.species.data
         photo_url = form.photo_url.data
         age = form.age.data
         notes = form.notes.data
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+        db.session.add(new_pet)
+        db.session.commit()
         flash(f"Added {name}")
-        return redirect('/add')
+        return redirect('/')
     else:
         return render_template('add_pet_form.html', form=form)
+
+############## EDIT PET FORM ##############
+
+@app.route('/<int:uid>', methods=["GET", "POST"])
+def view_edit_pet(uid):
+    """view and/or edit pet within the pets table"""
+    pet = Pet.query.get_or_404(uid)
+    form = EditPetInfo(obj=pet)
+    pet_photo_url = pet.photo_url
+    name = pet.name
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.add(pet)
+        db.session.commit()
+        flash(f"{name} updated!")
+        return redirect('/')
+    else:
+        return render_template('edit_pet_form.html', form=form, photo=pet.photo_url, name=name, species=pet.species)
 
--- forms.py
+++ forms.py
@@ -1,11 +1,45 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange, AnyOf
 
 ############## WTFORMS ##############
 
-class AddPet(FlaskForm):
-    name = StringField("Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo Url")
-    age = IntegerField("Age")
-    notes = StringField("Notes")
+class PetInfo(FlaskForm):
+    name = StringField("Name", 
+        validators=[
+            InputRequired(message="Name cannot be blank")
+            ])
+    species = StringField("Species", 
+        validators=[
+            InputRequired(message="Species cannot be blank"), 
+            AnyOf(values=["cat", "dog", "porcupine"], message="We only accept cats, dogs, or porcupines")
+            ])
+    photo_url = StringField("Photo URL", 
+        validators=[
+            Optional(strip_whitespace=True), 
+            URL(message="Photo URL must have a URL syntax")
+            ])
+    age = IntegerField("Age", 
+        validators=[
+            Optional(strip_whitespace=True), 
+            NumberRange(min=0, max=30, message="Age must be between 0 and 30")
+            ])
+    notes = StringField("Notes", 
+        validators=[
+            Optional(strip_whitespace=True)
+            ])
+
+class EditPetInfo(FlaskForm):
+    photo_url = StringField("Photo URL", 
+        validators=[
+            Optional(strip_whitespace=True), 
+            URL(message="Photo URL must have a URL syntax")
+            ])
+    notes = StringField("Notes", 
+        validators=[
+            Optional(strip_whitespace=True)
+            ])
+    available = BooleanField("Available", 
+        validators=[
+            Optional(strip_whitespace=True)
+            ])
